ad23ec0ded5daf0ae4f1740a2aff208ae28e4b58
artur <artur5mustafin@gmail.com>
2022-06-21 15:15:24 +0300

    fix

--- src/main/java/io/orionprotocol/Driver.java
+++ src/main/java/io/orionprotocol/Driver.java
@@ -5,30 +5,28 @@ import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.util.HashMap;
-import java.util.Map;
 
 
 //TODO: до делать
 public class Driver {
     public static void initDriver() {
 
-        Configuration.remote = "http://192.168.0.154:4444/wd/hub";
         Configuration.browser = "chrome";
-        Configuration.browserSize = "1920x1080";
 
-        Configuration.reportsFolder = "target/surefire-reports";
-        Configuration.downloadsFolder = "target/downloads";
-
-        Map<String, Boolean> options = new HashMap<>();
-        options.put("enableVNC", true);
-        options.put("enableVideo", false);
-        options.put("enableLog", false);
-
-        Configuration.browserCapabilities = new ChromeOptions();
-        Configuration.browserCapabilities.setCapability("selenoid:options", options);
+        //Configuration.remote = "http://192.168.0.154:4444/wd/hub";
+//        Configuration.browser = "chrome";
+//        Configuration.browserSize = "1920x1080";
+//
+//        Configuration.reportsFolder = "target/surefire-reports";
+//        Configuration.downloadsFolder = "target/downloads";
+//
+//        Map<String, Boolean> options = new HashMap<>();
+//        options.put("enableVNC", true);
+//        options.put("enableVideo", false);
+//        options.put("enableLog", false);
+//
+//        Configuration.browserCapabilities = new ChromeOptions();
+//        Configuration.browserCapabilities.setCapability("selenoid:options", options);
 
 //        DesiredCapabilities browser = new DesiredCapabilities();
 //        browser.setBrowserName("chrome");
